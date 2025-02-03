525c5db66f5e198bb393e3ffae29a2690acbeb22
KaczmarekJan <j.kaczmarek0608@gmail.com>
2025-02-03 17:37:13 +0000

    code optimization

--- script.js
+++ script.js
@@ -1,52 +1,34 @@
+//timekeeper
 function updatetime() {
-    const teraz = new Date();
-    const hours = teraz.getHours().toString().padStart(2, '0');
-    const minutes = teraz.getMinutes().toString().padStart(2, '0');
-    const seconds = teraz.getSeconds().toString().padStart(2, '0');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
     document.getElementById("time").innerText = hours+":"+minutes+":"+seconds;
 }
-
 setInterval(updatetime, 500);
 updatetime();
-document.addEventListener("mousemove", (e) => {
-    const background = document.querySelector(".background");
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    const mouseX = e.clientX / windowWidth - 0.5;
-    const mouseY = e.clientY / windowHeight - 0.5;
-    const offsetX = mouseX * 60;
-    const offsetY = mouseY * 60;
 
-    background.style.transform = `translate(calc(-50% + ${offsetX}px), calc(-50% + ${offsetY}px))`;
-});
-var gamerunning = false;
-function start() 
-{
-    gamerunning = true;
-    document.querySelector(".start_content").style.display = "none";
-    document.querySelector(".game_content").style.display = "flex";
-    game();
-}
-async function game()
-{
-    let level = (document.querySelector("#level").value)*2;
-    let points = 0;
-    let g = Math.floor(Math.random()*1)
-    switch(g)
+//moving background
+document.addEventListener("mousemove", (e) => 
     {
-        case 0:
-                colours();
-                break;
-                
-    }
-}
+        document.getElementById("background").style.transform = 
+        "translate(calc(-50% + " + ((e.clientX / window.innerWidth - 0.5) * 60) + "px), " + 
+                    "calc(-50% + " + ((e.clientY / window.innerHeight - 0.5) * 60) + "px))";
+    });
+
+//next page
 var m_next = 2;
 var m_previous = 1;
-function next() {
-    if (m_next == 3) {
+function next() 
+{
+    if (m_next == 3) 
+    {
         m_next = 1;
         m_previous = 3;
-    } else {
+    } 
+    else 
+    {
         m_previous = m_next;
         m_next += 1;
     }
@@ -54,6 +36,39 @@ function next() {
     document.getElementById("m" + m_next).style.display = "block";
     updatetitle(m_next);
 }
+
+
+//update page title
+function updatetitle(m_next)
+{
+    let text = "error";
+    switch (m_next)
+    {
+        case 1:
+            {
+                text = "<strong>Article</strong>";
+                break;
+            }
+        case 2:
+            {
+                text = "<strong>Game</strong>";
+                break;
+            }
+        case 3:
+            {
+                text = "<strong>Ranking</strong>";
+                break;
+            }
+        default:
+            {
+                text = "Another Error";
+                break;
+            }
+    }
+    document.getElementById("title").innerHTML = text;
+}
+
+//previous page
 function previous() {
     if (m_next == 1) {
         m_next = 3;
@@ -66,34 +81,28 @@ function previous() {
     document.getElementById("m" + m_next).style.display = "block";
     updatetitle(m_next);
 }
-function updatetitle(m_next)
-{
-    let text = "error";
-    switch (m_next)
-    {
-        case 1:
-            {
-                text = "How Memory Training Helps Different Conditions";
-                break;
-            }
-        case 2:
-            {
-                text = "Game";
-                break;
-            }
-        case 3:
-            {
-                text = "Ranking";
-                break;
-            }
-        default:
-            {
-                text = "Another Error";
-                break;
-            }
-    }
-    document.querySelector("#card_title").innerText = text;
-}
 
+
+
+//random game
+function game()
+{
+    if(document.getElementById("m2").style.display === "block")
+    {
+        document.getElementById("baner").style.visibility = "hidden";
+        document.getElementById("m2").style.display = "none";
+        document.getElementById("game_content").style.display = "flex";
+
+        switch(Math.floor(Math.random()*1))
+        {
+            case 0:
+                    colours();
+                    break;
+            default:
+                    alert("wrong case number");
+                    break;
+        }
+    }
+}
 
 
