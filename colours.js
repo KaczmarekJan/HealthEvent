0af4f5bc04c85812e5efb3ed6d4513ec37bcb551
KaczmarekJan <j.kaczmarek0608@gmail.com>
2025-02-04 13:42:30 +0000

    Almost working colours system, console.log undefined variables...

--- colours.js
+++ colours.js
@@ -1,51 +1,101 @@
+//variables
 var blocks = Array.from(document.getElementsByClassName("colour")); //create a table from existing classes
 var required_count = new Array(4).fill(0);  //table with required amounts of clicks
+var block_sequence = new Array(4).fill(0);  //table with block sequence
 var count = new Array(4).fill(0);   //table with actual clicks
+var points = 0;
 
 
 //colours game
 async function colours()
 {
     document.getElementById("colours").style.display = "flex";    //show the blocks
-    const blocks_colours = [
-        "#FF5733", // Bright Red Orange
-        "#33FF57", // Neon Green
-        "#337BFF", // Bright Blue
-        "#FF33A8", // Intense Pink
-        "#FFF833", // Lemon Yellow
-        "#33FFF8", // Neon Turquoise
-        "#9D33FF", // Vibrant Purple
-        "#FF8C33", // Bright Orange
-    ];
-
-    for(let i=0;i<4;i++)    //loop that will go thru all 4 blocks and randomize its color
+    let number_of_blocks = 0;
+    let amount_of_blinks = 0;
+    for(let r=1;r<level_value;r++)
     {
-        document.getElementsByClassName("colour")[i].style.display = "block";   //show the block
-        let randomIndex = Math.floor(Math.random()*blocks_colours.length);  //randomize blocks colours
-        document.getElementsByClassName("colour")[i].style.backgroundColor = 
-        blocks_colours.splice(randomIndex, 1)[0];   //delete 1 element from table so that it wont be picked twice
-    }
-    for(let i=0;i<4;i++)    //loop goes thru all 4 blocks
-    {
-        let randomIndex = Math.floor(Math.random() * blocks.length);    //randomize blocks
-        let randomBlock = blocks.splice(randomIndex, 1)[0]; //adress the block and then delete it
-        let randomBlinks = Math.ceil(Math.random()*level_value);    //random amount of blinks
-
-        for(let j=0;j<randomBlinks;j++) //loop makes blocks blink
+        const blocks_colours = [
+            "#FF5733", // Bright Red Orange
+            "#33FF57", // Neon Green
+            "#337BFF", // Bright Blue
+            "#FF33A8", // Intense Pink
+            "#FFF833", // Lemon Yellow
+            "#33FFF8", // Neon Turquoise
+            "#9D33FF", // Vibrant Purple
+            "#FF8C33", // Bright Orange
+        ];
+        var blocks_copy = Array.from(blocks);
+        for(let i=0;i<4;i++)    //loop that will go thru all 4 blocks and randomize its color
         {
-            randomBlock.style.boxShadow = `0 0 10px 10px ${randomBlock.style.backgroundColor}`; //display shadow
-            await delay(500);   //wait
-            randomBlock.style.boxShadow = "none";   //hide shadow
-            await delay(500);   //wait
+            document.getElementsByClassName("colour")[i].style.display = "block";   //show the block
+            let randomIndex = Math.floor(Math.random()*blocks_colours.length);  //randomize blocks colours
+            document.getElementsByClassName("colour")[i].style.backgroundColor = 
+            blocks_colours.splice(randomIndex, 1)[0];   //delete 1 element from table so that it wont be picked twice
         }
-        required_count[i] = randomBlinks;   //adress required amount of clicks
+        for(let i=0;i<number_of_blocks;i++)    //loop goes thru all 4 blocks
+        {
+            let randomIndex = Math.floor(Math.random() * blocks_copy.length);    //randomize blocks
+            let randomBlock = blocks_copy.splice(randomIndex, 1)[0]; //adress the block and then delete it
+            let randomBlinks = Math.ceil(Math.random() * amount_of_blinks);    //random amount of blinks
+    
+            for(let j=0;j<randomBlinks;j++) //loop makes blocks blink
+            {
+                await delay(500);   //wait
+                randomBlock.style.boxShadow = `0 0 5px 5px ${randomBlock.style.backgroundColor}`; //display shadow
+                await delay(500);   //wait
+                randomBlock.style.boxShadow = "none";   //hide shadow
+                await delay(500);   //wait
+            }
+            required_count[i] = randomBlinks;
+            block_sequence[i] = randomBlock;    //adress required amount of clicks
+        }
+            let startTime = Date.now();
+            let p=-1;
+            while(p<4)
+                {
+                    console.log(required_count[p]);
+                    console.log(count[block_sequence[p]]);
+                    p++;
+                    if(required_count[p]===count[block_sequence[p]])
+                    {
+                        points++;
+                        break;
+                    }
+                    else if(required_count[p]<count[block_sequence[p]])
+                    {
+                        alert("Wrong");
+                        break;
+                    }
+                    else if(Date.now()-startTime >= 5000)
+                    {
+                        alert("Too long");
+                        break;
+                    }
+                    else
+                    {
+                        await delay(1000);
+                        p=-1;
+                    }
+                }
+        amount_of_blinks++;
+        if(number_of_blocks<4)
+        {
+            number_of_blocks++;
+        }
     }
-    answer();   //function that will handle counting and checking users clicks
 }
 
 
 //user clicks on the blocks
-function answer() 
+async function users_click(x) 
 {
-
+    if(x !== undefined)
+    {
+        const colour = document.getElementsByClassName("colour")[x];
+        colour.style.boxShadow = `0 0 5px 5px ${colour.style.backgroundColor}`; //display shadow
+        await delay(500);   //wait
+        colour.style.boxShadow = "none";   //hide shadow
+        await delay(500);   //wait
+        count[x]++;
+    }
 }
