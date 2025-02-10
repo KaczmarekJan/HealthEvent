56613e27476008d2aa35df4b7fe6c5a74bc21990
KaczmarekJan <j.kaczmarek0608@gmail.com>
2025-02-10 21:59:33 +0000

    numbers lvl 1 and 2

--- numbers.js
+++ numbers.js
@@ -2,53 +2,100 @@ async function numbers()
 {
     document.getElementById("numbers").style.display = "flex";
     const content = document.getElementById("content");
-    content.innerHTML = "Sum all the numbers";
-    await delay(1000);
     const numbers = [];
     for(let i = 0; i < 100; i++) {numbers.push(i);}
     let amount_of_numbers = 3;
     let numbers_range = 90;
-    for(let r=0; r<level_value*3; r++)
+    for(let r=0; r<level_value*2; r++)
     {
-        let equals = 0;
-        for(let i=0; i<amount_of_numbers; i++)
+        if(level_value == 3 )
         {
-            let RandomIndex = Math.ceil(Math.random()*(numbers.length-numbers_range));
-            equals += numbers[RandomIndex];
-            content.innerHTML = numbers.splice(RandomIndex, 1)[0];
+            content.innerHTML = "Sum all the numbers";
             await delay(1000);
-        }
-        content.innerHTML = `<input type="number" id="userAnswer" placeholder="Enter">`;
-        await new Promise(resolve => {
-            const userAnswer = document.getElementById("userAnswer");
-            const checkAnswer = async (event) =>
-                {
-                    if(event.key === "Enter")
+            let equals = 0;
+            for(let i=0; i<amount_of_numbers; i++)
+            {
+                let RandomIndex = Math.ceil(Math.random()*(numbers.length-numbers_range));
+                equals += numbers[RandomIndex];
+                content.innerHTML = numbers.splice(RandomIndex, 1)[0];
+                await delay(1000);
+            }
+            content.innerHTML = `<input type="number" id="userAnswer" placeholder="Enter">`;
+            await new Promise(resolve => {
+                const userAnswer = document.getElementById("userAnswer");
+                const checkAnswer = async (event) =>
                     {
-                        if(userAnswer.value == equals)
-                            {
-                                content.innerHTML = "Correct!";
-                                await delay(1000);
-                                points++;
-                                resolve();
-                            }
-                        else
-                            {
-                                content.innerHTML = "Incorrect!";
-                                await delay(1000);
-                                resolve();
-                            }
+                        if(event.key === "Enter")
+                        {
+                            if(userAnswer.value == equals)
+                                {
+                                    content.innerHTML = "Correct!";
+                                    await delay(1000);
+                                    points++;
+                                    resolve();
+                                }
+                            else
+                                {
+                                    content.innerHTML = "Incorrect!";
+                                    await delay(1000);
+                                    resolve();
+                                }
+                        }
                     }
+                userAnswer.addEventListener("keydown", checkAnswer);
+            });
+        }
+        else
+        {
+            content.innerHTML = "Remember all the numbers";
+            await delay(1000);
+            var equals = new Array(amount_of_numbers).fill(0);
+            for(let i=0; i<amount_of_numbers/5; i++)
+            {
+                let RandomIndex = Math.ceil(Math.random()*(numbers.length-numbers_range));
+                equals[i] = numbers[RandomIndex];
+                content.innerHTML = numbers.splice(RandomIndex, 1)[0];
+                await delay(1000);
+            }
+            content.innerHTML = `<input type="number" id="userAnswer" placeholder="Enter numbers 1 to ${amount_of_numbers}">`;
+            var answer = new Array(amount_of_numbers).fill(0);
+            await new Promise(resolve => {
+                const userAnswer = document.getElementById("userAnswer");
+                let i = 0;
+                const checkAnswer = async (event) =>
+                    {
+                        if(event.key === "Enter")
+                        {
+                            answer[i] = userAnswer.value;
+                            userAnswer.value = "";
+                            if(i == amount_of_numbers-1)
+                            {
+                                userAnswer.removeEventListener("keydown", checkAnswer);
+                                resolve();
+                            }
+                            i++;
+                        }
+                    }
+                userAnswer.addEventListener("keydown", checkAnswer);
+            });
+            if(answer.every((val, idx) => val == equals[idx]))
+                {
+                    content.innerHTML = "Correct!";
+                    await delay(1000);
+                    points++;
                 }
-            userAnswer.addEventListener("keydown", checkAnswer);
-        });
-
+            else
+                {
+                    content.innerHTML = "Incorrect!";
+                    await delay(1000);
+                }
+        }
         if(numbers_range > 9)
-            numbers_range -= 10;
+            numbers_range -= 5;
         else
             numbers_range = 0;
 
-        amount_of_numbers ++;
+        amount_of_numbers += 0.5;
     }
     content.innerHTML = `Score: ${points}`;
     await delay(2000);
