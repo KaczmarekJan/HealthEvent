a63697eb3c0cb863f02374f17ca66f74bd83eceb
Serafinczyk <129699458+Serafinczyk@users.noreply.github.com>
2025-02-11 21:39:55 +0000

    level value standarised

--- memory.js
+++ memory.js
@@ -3,80 +3,79 @@ async function memory(){
     var number_of_images = 40; //number of images in folder
      
     document.getElementById("memory").style.display = "flex"; //display game
-    for(let level = 0;level<3;level++){
 
-        //Generate random blocks
-        console.log("Generating random blocks:");
-        let elements_number = Math.pow((level*2)+4,2);
-        let images = Array();
-        for(let i = 0; i<elements_number;i+=2){
-            let random = 0;
-            do{
-                random = Math.floor(Math.random() * number_of_images);
-            }while(images.includes(random));
-            images[i] = random;
-            images[i+1] = random;
-        }
-        
-        //Shuffle array
-        for (var i = images.length - 1; i >= 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = images[i];
-            images[i] = images[j];
-            images[j] = temp;
-        }
-        console.log(images);
+    let level = level_value - 1;
+    //Generate random blocks
+    console.log("Generating random blocks:");
+    let elements_number = Math.pow((level*2)+4,2);
+    let images = Array();
+    for(let i = 0; i<elements_number;i+=2){
+        let random = 0;
+        do{
+            random = Math.floor(Math.random() * number_of_images);
+        }while(images.includes(random));
+        images[i] = random;
+        images[i+1] = random;
+    }
+    
+    //Shuffle array
+    for (var i = images.length - 1; i >= 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = images[i];
+        images[i] = images[j];
+        images[j] = temp;
+    }
+    console.log(images);
 
-        //Render view
-        var tiles = Array(elements_number).fill(null).map(() => {
-            var element = document.createElement("div");
-            element.classList.add("memory-tile");
-            document.getElementById("memory").appendChild(element);
-            return element;
-        });
+    //Render view
+    var tiles = Array(elements_number).fill(null).map(() => {
+        var element = document.createElement("div");
+        element.classList.add("memory-tile");
+        document.getElementById("memory").appendChild(element);
+        return element;
+    });
 
-        var remainigTiles = elements_number/2;
-        await new Promise(resolve => {
-            var lastTile = null;
-            const handleClick = async (event) => {
-                
-                tiles.forEach((element) => { //block event listener during animation
-                    element.removeEventListener("click", handleClick);
-                });
+    var remainigTiles = elements_number/2;
+    await new Promise(resolve => {
+        var lastTile = null;
+        const handleClick = async (event) => {
+            
+            tiles.forEach((element) => { //block event listener during animation
+                element.removeEventListener("click", handleClick);
+            });
 
-                const clickedTile = event.target;
-                const tileIndex = tiles.indexOf(clickedTile);
-                console.log("Clicked tile: " + tileIndex + "=>"+images[tileIndex]);
-                clickedTile.style.backgroundImage = `url('memory/${images[tileIndex]}.jpg')`;
-                
-                if (lastTile === null || lastTile === tileIndex) { //first click
-                    lastTile = tileIndex;
-                } else {
-                   await delay(1000);
-                   if(images[tileIndex] === images[lastTile]){ //pair found
-                          clickedTile.style.visibility = "hidden";
-                          tiles[lastTile].style.visibility = "hidden";
-                          remainigTiles--;
-                   }else{ //pair not found
-                            clickedTile.style.backgroundImage = "";
-                            tiles[lastTile].style.backgroundImage = "";
-                   }
-                   lastTile=null;
+            const clickedTile = event.target;
+            const tileIndex = tiles.indexOf(clickedTile);
+            console.log("Clicked tile: " + tileIndex + "=>"+images[tileIndex]);
+            clickedTile.style.backgroundImage = `url('memory/${images[tileIndex]}.jpg')`;
+            
+            if (lastTile === null || lastTile === tileIndex) { //first click
+                lastTile = tileIndex;
+            } else {
+                await delay(1000);
+                if(images[tileIndex] === images[lastTile]){ //pair found
+                        clickedTile.style.visibility = "hidden";
+                        tiles[lastTile].style.visibility = "hidden";
+                        remainigTiles--;
+                }else{ //pair not found
+                        clickedTile.style.backgroundImage = "";
+                        tiles[lastTile].style.backgroundImage = "";
                 }
-                if(remainigTiles === 0){ //all pairs found game over
-                    resolve();
-                }
-
-                tiles.forEach((element) => { //restore after animation
-                    element.addEventListener("click", handleClick);
-                });
+                lastTile=null;
             }
-            tiles.forEach((element) => {
+            if(remainigTiles === 0){ //all pairs found game over
+                resolve();
+            }
+
+            tiles.forEach((element) => { //restore after animation
                 element.addEventListener("click", handleClick);
             });
+        }
+        tiles.forEach((element) => {
+            element.addEventListener("click", handleClick);
         });
+    });
 
-        document.getElementById("memory").innerHTML = "";
-    }
+    document.getElementById("memory").innerHTML = "";
     document.getElementById("memory").style.display = "none";
 }
