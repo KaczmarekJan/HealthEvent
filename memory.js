27a36c8f414eaab19f98988d72a1945145c0f55a
Serafinczyk <129699458+Serafinczyk@users.noreply.github.com>
2025-02-11 21:25:03 +0000

    memorki full beta auuuuu

--- memory.js
+++ memory.js
@@ -34,12 +34,42 @@ async function memory(){
             document.getElementById("memory").appendChild(element);
             return element;
         });
+
+        var remainigTiles = elements_number/2;
         await new Promise(resolve => {
+            var lastTile = null;
             const handleClick = async (event) => {
+                
+                tiles.forEach((element) => { //block event listener during animation
+                    element.removeEventListener("click", handleClick);
+                });
+
                 const clickedTile = event.target;
                 const tileIndex = tiles.indexOf(clickedTile);
-                console.log("Clicked tile: " + tileIndex);
+                console.log("Clicked tile: " + tileIndex + "=>"+images[tileIndex]);
                 clickedTile.style.backgroundImage = `url('memory/${images[tileIndex]}.jpg')`;
+                
+                if (lastTile === null || lastTile === tileIndex) { //first click
+                    lastTile = tileIndex;
+                } else {
+                   await delay(1000);
+                   if(images[tileIndex] === images[lastTile]){ //pair found
+                          clickedTile.style.visibility = "hidden";
+                          tiles[lastTile].style.visibility = "hidden";
+                          remainigTiles--;
+                   }else{ //pair not found
+                            clickedTile.style.backgroundImage = "";
+                            tiles[lastTile].style.backgroundImage = "";
+                   }
+                   lastTile=null;
+                }
+                if(remainigTiles === 0){ //all pairs found game over
+                    resolve();
+                }
+
+                tiles.forEach((element) => { //restore after animation
+                    element.addEventListener("click", handleClick);
+                });
             }
             tiles.forEach((element) => {
                 element.addEventListener("click", handleClick);
