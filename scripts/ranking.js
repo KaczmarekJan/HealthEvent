1d627ffd4d8e6839fffb820858bff6c6e7499226
Serafinczyk <129699458+Serafinczyk@users.noreply.github.com>
2025-02-16 00:42:10 +0000

    ranking start

--- scripts/ranking.js
+++ scripts/ranking.js
@@ -1,4 +1,7 @@
-function registerNewNick (nick){
-
+const request = new Request("https://example.com", {
+  method: "POST",
+  body: '{"id": "0"}', // tym się nie przejmuj, użyjemy później
+});
+function getRanking() {
+    
 }
-
